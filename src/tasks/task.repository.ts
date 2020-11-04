044cc1aae2d59911f1b808dd6f8c96e6807a50ca
Noah Thorne <48963204+noahlthorne@users.noreply.github.com>
2020-11-04 11:43:24 +0900

    added get tasks to repository

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -2,9 +2,14 @@ import { Repository, EntityRepository } from "typeorm";
 import { Task } from "./task.entity";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { TaskStatus } from "./task-status.enum";
+import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    const { status, search } =  filterDto;
+  }
+
   async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
     const { title, description} = createTaskDto
     const task = new Task();
