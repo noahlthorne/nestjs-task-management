1e331491953bf51aa8050dad8442a500e8aaf6d7
Noah Thorne <48963204+noahlthorne@users.noreply.github.com>
2020-11-06 11:57:57 +0900

    added status and seach database queries

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -8,6 +8,18 @@ import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
 export class TaskRepository extends Repository<Task> {
   async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
     const { status, search } =  filterDto;
+    const query = this.createQueryBuilder('task');
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+
+    if (search) {
+      query.andWhere('(task.title LIKE :search OR task.description LIKE :search)', { search: `%${search}%` });
+      console.log(search);
+    }
+
+    const tasks = await query.getMany();
+    return tasks;
   }
 
   async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
