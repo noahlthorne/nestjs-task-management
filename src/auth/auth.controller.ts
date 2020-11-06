a8577d5df9e48435baf2e3f50549075e26b4e6c9
Noah Thorne <48963204+noahlthorne@users.noreply.github.com>
2020-11-06 13:16:54 +0900

    add signup path to controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+  @Post('/signup')
+  signUp(@Body() authCredentialsDto: AuthCredentialsDto) {
+    console.log(authCredentialsDto);
+  }
+}
