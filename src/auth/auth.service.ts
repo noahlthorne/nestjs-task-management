cb98eca2630f6b0ba52da1a19aa096e9f69f4011
Noah Thorne <48963204+noahlthorne@users.noreply.github.com>
2020-11-10 16:15:23 +0900

    add sign in method to auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
-import { InjectRepository } from '@nestjs/typeorm'
+import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
@@ -12,5 +12,10 @@ export class AuthService {
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signUp(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto) {
+    const result = await this.userRepository.validateUserPassword(authCredentialsDto);
+    console.log(result);
   }
 }
