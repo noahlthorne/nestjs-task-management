b06371ac3798a2a014d4f689ec673fd8cd38c1b5
Noah Thorne <48963204+noahlthorne@users.noreply.github.com>
2020-11-10 11:11:59 +0900

    add validate password to user repository

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,8 +1,11 @@
-import { Repository, EntityRepository } from "typeorm";
-import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import { Repository, EntityRepository } from 'typeorm';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
-import { User } from "./user.entity";
-import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -22,6 +25,18 @@ export class UserRepository extends Repository<User> {
       } else {
         throw new InternalServerErrorException();
       }
+    }
+  }
+
+  async validateUserPassword(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.findOne({ username });
+    if (user && (await user.validatePassword(password))) {
+      return user.username;
+    } else {
+      return null;
     }
   }
 
